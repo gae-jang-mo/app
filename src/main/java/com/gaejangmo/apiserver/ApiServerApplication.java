3d50ed79965ab04a2f6c15476013d3897fc91ab6
JunHoPark <pjh374968@gmail.com>
2019-11-26 16:08:46 +0900

    feat: 초기 세팅

---
+++ src/main/java/com/gaejangmo/apiserver/ApiServerApplication.java
@@ -0,0 +1,13 @@
+package com.gaejangmo.apiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gaejangmo/apiserver/ApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaejangmo.apiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
