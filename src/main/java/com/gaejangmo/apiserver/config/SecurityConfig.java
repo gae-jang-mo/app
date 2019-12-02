519172b4db813bb2c9f620d3f00f460c3b18f8d9
DaejunBae <dpudpu11@gmail.com>
2019-12-02 16:27:26 +0900

    feat: 시큐리티 설정 적용

---
+++ src/main/java/com/gaejangmo/apiserver/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.gaejangmo.apiserver.config;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(final WebSecurity web) throws Exception {
+        web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated();
+
+        http.httpBasic();
+        http.csrf().disable();
+    }
+}
