ce17b8a4a01706e8992b1081cd39dd045f673499
JunHoPark <pjh374968@gmail.com>
2019-12-02 16:56:27 +0900

    feat: BaseEntity 정의

---
+++ src/main/java/com/gaejangmo/apiserver/domain/common/BaseEntity.java
@@ -0,0 +1,27 @@
+package com.gaejangmo.apiserver.domain.common;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@EqualsAndHashCode(of = "id")
+@MappedSuperclass
+@EntityListeners(value = AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime lastModifiedAt;
+}
