af4061173f2260b43ccc6b48051d63b5a70bb5d2
박효진 <hgenie14@gmail.com>
2019-12-10 19:50:19 +0900

    test: 테스트 환경설정 수정

--- src/test/java/com/gaejangmo/apiserver/ApiServerApplicationTests.java
+++ src/test/java/com/gaejangmo/apiserver/ApiServerApplicationTests.java
@@ -2,8 +2,10 @@ package com.gaejangmo.apiserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("dev")
 class ApiServerApplicationTests {
 
     @Test
