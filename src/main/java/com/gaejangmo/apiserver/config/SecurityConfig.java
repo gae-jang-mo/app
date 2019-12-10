815df4fa0d5355c5fcd3a85087a0812443a92d25
박효진 <hgenie14@gmail.com>
2019-12-10 21:19:02 +0900

    feat: SecurityConfig 클래스 수정

--- src/main/java/com/gaejangmo/apiserver/config/SecurityConfig.java
+++ src/main/java/com/gaejangmo/apiserver/config/SecurityConfig.java
@@ -23,7 +23,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/", "/api/login/state", "/h2-console/**").permitAll()
+                //.antMatchers("/**").permitAll()
+                .antMatchers("/", "/api/login/state", "/h2-console/**", "/oauth2/redirect").permitAll()
                 .anyRequest().authenticated();
 
         http.httpBasic();
