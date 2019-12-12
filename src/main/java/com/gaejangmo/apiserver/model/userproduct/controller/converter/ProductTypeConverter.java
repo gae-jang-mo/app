6f2bfd700187c8d2689a53cd528d13df5f9b2689
DaejunBae <dpudpu11@gmail.com>
2019-12-12 20:57:16 +0900

    style: TODO 제거

--- src/main/java/com/gaejangmo/apiserver/model/userproduct/controller/converter/ProductTypeConverter.java
+++ src/main/java/com/gaejangmo/apiserver/model/userproduct/controller/converter/ProductTypeConverter.java
@@ -4,7 +4,6 @@ import com.gaejangmo.apiserver.model.userproduct.domain.vo.ProductType;
 import org.springframework.core.convert.converter.Converter;
 
 
-// TODO: 2019/12/10 위치 여기하는게 맞을까?
 public class ProductTypeConverter implements Converter<String, ProductType> {
 
     @Override
