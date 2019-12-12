d94492a1e990302e653e72360b90e02a404e30af
DaejunBae <dpudpu11@gmail.com>
2019-12-12 14:34:29 +0900

    refactor: UserProductResponseDto.productType 클래스 타입 변경

--- src/main/java/com/gaejangmo/apiserver/model/userproduct/service/dto/UserProductResponseDto.java
+++ src/main/java/com/gaejangmo/apiserver/model/userproduct/service/dto/UserProductResponseDto.java
@@ -1,6 +1,5 @@
 package com.gaejangmo.apiserver.model.userproduct.service.dto;
 
-import com.gaejangmo.apiserver.model.userproduct.domain.vo.ProductType;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -15,7 +14,7 @@ import java.time.LocalDateTime;
 public class UserProductResponseDto {
     private Long id;
     private Long productId;
-    private ProductType productType;
+    private String productType;
     private String comment;
     private LocalDateTime createdAt;
     private String imageUrl;
