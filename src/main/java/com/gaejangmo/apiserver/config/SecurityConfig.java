c9aad6a40f98cd710adc32a13f866272c16191d3
seonghun127 <seonghun127@gmail.com>
2019-12-12 11:21:03 +0900

    chore: 주석 제거

--- src/main/java/com/gaejangmo/apiserver/config/SecurityConfig.java
+++ src/main/java/com/gaejangmo/apiserver/config/SecurityConfig.java
@@ -23,7 +23,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                //.antMatchers("/**").permitAll()
                 .antMatchers("/", "/api/login/state", "/h2-console/**", "/oauth2/redirect").permitAll()
                 .anyRequest().authenticated();
 
