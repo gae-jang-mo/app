9d185f8e8459d549a98d0726b7ef5cfe5621e109
seonghun127 <seonghun127@gmail.com>
2019-12-12 18:02:32 +0900

    feat: UserApiController 추가

---
+++ src/main/java/com/gaejangmo/apiserver/model/user/controller/UserApiController.java
@@ -0,0 +1,28 @@
+package com.gaejangmo.apiserver.model.user.controller;
+
+import com.gaejangmo.apiserver.model.user.domain.User;
+import com.gaejangmo.apiserver.model.user.dto.UserResponseDto;
+import com.gaejangmo.apiserver.model.user.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserApiController {
+    private final UserService userService;
+
+    public UserApiController(final UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/logined")
+    public ResponseEntity<UserResponseDto> find(HttpSession session) {
+        User user = (User) session.getAttribute("user");
+        UserResponseDto response = userService.findUserResponseDtoByUsername(user.getUsername());
+        return ResponseEntity.ok().body(response);
+    }
+}
