3d2769c6bee656caf1b3f544ed22d38cc8b3e435
JunHoPark <pjh374968@gmail.com>
2019-12-17 16:24:36 +0900

    fix: OAuth redirect url

--- src/main/java/com/gaejangmo/apiserver/config/SecurityConfig.java
+++ src/main/java/com/gaejangmo/apiserver/config/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.headers().frameOptions().disable();
 
         http.oauth2Login()
-                .defaultSuccessUrl("http://localhost:8082/")
+                .defaultSuccessUrl("http://15.165.16.245:8082/")
                 .userInfoEndpoint()
                 .userService(customOAuth2UserService);
     }
