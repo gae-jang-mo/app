86167ae2d83abb2e40a6f93c76041e08e789a6b2
JunHoPark <pjh374968@gmail.com>
2019-12-17 16:23:15 +0900

    fix: redirect url 뒤에 슬래시 추가

--- src/main/java/com/gaejangmo/apiserver/config/SecurityConfig.java
+++ src/main/java/com/gaejangmo/apiserver/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.gaejangmo.apiserver.config;
 
 import com.gaejangmo.apiserver.config.oauth.service.CustomOAuth2UserService;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -10,7 +9,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
-@Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final CustomOAuth2UserService customOAuth2UserService;
 
@@ -38,7 +36,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.headers().frameOptions().disable();
 
         http.oauth2Login()
-                .defaultSuccessUrl("http://localhost:8082")
+                .defaultSuccessUrl("http://localhost:8082/")
                 .userInfoEndpoint()
                 .userService(customOAuth2UserService);
     }
