06412c5ded1293daf2dc3476a0815cccb45303cf
Hyo-Jin Park <hgenie14@gmail.com>
2019-12-18 11:09:41 +0900

    Update src/main/java/com/gaejangmo/apiserver/model/user/domain/UserRepository.java

--- src/main/java/com/gaejangmo/apiserver/model/user/domain/UserRepository.java
+++ src/main/java/com/gaejangmo/apiserver/model/user/domain/UserRepository.java
@@ -6,6 +6,5 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByOauthId(Long oauthId);
-    Optional<User> findById(Long id);
     Optional<User> findByUsername(String username);
 }
