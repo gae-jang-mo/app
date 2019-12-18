d481d61a296bdf002de910c6c9269840742f0363
seonghun127 <seonghun127@gmail.com>
2019-12-18 14:19:12 +0900

    refactor: @WithMockCustomUSer 어노테이션에 role 필드 추가

--- src/test/java/com/gaejangmo/apiserver/model/common/support/WithMockCustomUserSecurityContextFactory.java
+++ src/test/java/com/gaejangmo/apiserver/model/common/support/WithMockCustomUserSecurityContextFactory.java
@@ -3,9 +3,12 @@ package com.gaejangmo.apiserver.model.common.support;
 import com.gaejangmo.apiserver.config.oauth.SecurityUser;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.test.context.support.WithSecurityContextFactory;
+
+import java.util.Set;
 
 public class WithMockCustomUserSecurityContextFactory
         implements WithSecurityContextFactory<WithMockCustomUser> {
@@ -19,6 +22,7 @@ public class WithMockCustomUserSecurityContextFactory
                 .oauthId(Long.valueOf(customUser.oauthId()))
                 .username(customUser.username())
                 .email(customUser.email())
+                .authorities(Set.of(new SimpleGrantedAuthority(customUser.role())))
                 .build();
 
         Authentication auth =
