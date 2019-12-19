c9a3bfecd786a01afb9e66580a5a765fde178e77
DaejunBae <dpudpu11@gmail.com>
2019-12-19 14:42:33 +0900

    feat: MockMvcTest UTF-8 인코딩 필터 추가

--- src/test/java/com/gaejangmo/apiserver/model/MockMvcTest.java
+++ src/test/java/com/gaejangmo/apiserver/model/MockMvcTest.java
@@ -9,6 +9,7 @@ import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
@@ -29,6 +30,7 @@ public abstract class MockMvcTest {
     protected void setUp(WebApplicationContext webApplicationContext,
                          RestDocumentationContextProvider restDocumentation) {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .addFilters(new CharacterEncodingFilter("UTF-8", true))
                 .apply(documentationConfiguration(restDocumentation))
                 .apply(springSecurity())
                 .build();
