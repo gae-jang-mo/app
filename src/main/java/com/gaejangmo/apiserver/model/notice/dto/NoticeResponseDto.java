7b20671e59700f1b55f7fb0511a2d62e77d92b66
JunHoPark <pjh374968@gmail.com>
2019-12-20 13:21:45 +0900

    refactor: NoticeResponseDto 애노테이션 정리

--- src/main/java/com/gaejangmo/apiserver/model/notice/dto/NoticeResponseDto.java
+++ src/main/java/com/gaejangmo/apiserver/model/notice/dto/NoticeResponseDto.java
@@ -3,15 +3,20 @@ package com.gaejangmo.apiserver.model.notice.dto;
 import com.gaejangmo.apiserver.model.notice.domain.NoticeType;
 import lombok.*;
 
-@NoArgsConstructor
-@AllArgsConstructor
 @Getter
-@Setter
-@Builder
 @EqualsAndHashCode
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class NoticeResponseDto {
     private Long id;
     private NoticeType noticeType;
     private String header;
     private String contents;
+
+    @Builder
+    public NoticeResponseDto(final Long id, final NoticeType noticeType, final String header, final String contents) {
+        this.id = id;
+        this.noticeType = noticeType;
+        this.header = header;
+        this.contents = contents;
+    }
 }
