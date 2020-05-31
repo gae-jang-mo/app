4f97f7924420c0a9da95ff7be2329ccc5da67f69
JunHoPark93 <pjh374968@gmail.com>
2020-05-31 15:50:56 +0900

    refactor: 장비 검색 서버 jackson 설정으로 인한 대응

--- src/main/java/com/gaejangmo/apiserver/model/product/dto/NaverProductResponseDto.java
+++ src/main/java/com/gaejangmo/apiserver/model/product/dto/NaverProductResponseDto.java
@@ -1,5 +1,6 @@
 package com.gaejangmo.apiserver.model.product.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.gaejangmo.apiserver.model.product.domain.vo.NaverProductType;
 import lombok.*;
 
@@ -10,12 +11,27 @@ import lombok.*;
 @Builder
 @EqualsAndHashCode
 public class NaverProductResponseDto {
+    @JsonProperty("title")
     private String productName;
+
+    @JsonProperty("link")
     private String buyUrl;
+
+    @JsonProperty("image")
     private String imageUrl;
+
+    @JsonProperty("lprice")
     private long lowestPrice;
+
+    @JsonProperty("hprice")
     private long highestPrice;
+
+    @JsonProperty("mallName")
     private String mallName;
+
+    @JsonProperty("productId")
     private long productId;
+
+    @JsonProperty("productType")
     private NaverProductType naverProductType;
 }
