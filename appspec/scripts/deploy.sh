3723f2be82b4985a989554c973e1825abcf8a23b
배대준 <dpudpu11@gmail.com>
2020-05-31 13:53:25 +0900

    Update deploy.sh

--- appspec/scripts/deploy.sh
+++ appspec/scripts/deploy.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-REPOSITORY=/home/ec2-user/app-server/deploy/zip
+REPOSITORY=/home/ec2-user/app-server/deploy
+PROJECT_NAME=api-server
 
-cd $REPOSITORY/
+cd $REPOSITORY
+
+echo "> build 파일 복사"
+cp $REPOSITORY/zip/*.jar $REPOSITORY/$PROJECT_NAME
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
-CURRENT_PID=$(ps -ef | grep java | grep api-server* | awk '{print $2}')
-echo "$CURRENT_PID"
+CURRENT_PID=$(pgrep -fl $PROJECT_NAME)
+echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
 if [ -z $CURRENT_PID ]; then
   echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
@@ -24,3 +28,10 @@ chmod +x $JAR_NAME
 
 echo "> 개장모 메인 서버 배포 합니다 !!"
 nohup java -jar -Dspring.profiles.active=dev /home/ec2-user/app-server/deploy/zip/api-server-0.0.1-SNAPSHOT.jar >> /home/ec2-user/app-server/logs/api-server.log &
+
+echo "> 새 애플리케이션 배포"
+JAR_NAME=$(ls -tr $REPOSITORY/ |grep *.jar | tail -n 1)
+echo ">Jar Name: $JAR_NAME"
+
+echo ">$JAR_NAME 에 실행권한 추가"
+chmod +x $JAR_NAME
